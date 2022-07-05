d490a050fa568ef41e3cac122603ae5a066c2cdc
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-07-05 10:28:59 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,137 @@
 import {useState, useEffect} from 'react';
 import './App.css';
-import ToggleDivImage from './components/ToggleImg';
 import axios from 'axios'
+import ToggleDivImage from './components/ToggleImg';
+import NewProfile from './components/NewProfile';
+
+
 
 
 
 
 function App() {
-  const [userAccounts, setUserAccounts] = useState([]) // user accounts from the backend
 
-    // Testing route to get user accounts
-  //   const getUserAccounts = () => {
-  //     axios.get('https://twentyfiveftfromtherim.herokuapp.com/profile')
-  //     //axios.get("http://localhost:8000/api/books")
-  //       .then(response => setUserAccounts(response.data),
-  //           err => console.log(err)
-  //       ).catch(error => console.error(error))
-  // }
-  
-    const getUserAccounts = () => {
-      axios.get('https://www.balldontlie.io/api/v1/stats')
-      //axios.get("http://localhost:8000/api/books")
-        .then(response => setUserAccounts(response.data),
-            err => console.log(err)
-        ).catch(error => console.error(error))
+const [profiles, setProfiles] = useState([]) 
+//profiles from the backend
+const [loggedIn, setLoggedIn] = useState({}) 
+//profiles from the backend
+
+const [signedIn, setSignedIn] = useState(true) 
+//temp. for testing purposes
+const [showSignIn, setShowSignIn] = useState(false)
+
+
+const [allPlayers, setAllPlayers] = useState([]) 
+// players from ball don't lie api
+
+  //sign in toggle
+  const signInToggle = () => {
+    if (showSignIn === false) {
+      setShowSignIn(true)
+      // setShowAddForm(false)
+      // setShowSearch(false)
+      // setShowCart(false)
+    } else {
+      setShowSignIn(false)
+    }
   }
 
-  //Gets all books then loads page
+//route to get profiles
+const getProfiles = () => {
+    axios.get('https://twentyfiveftfromtherim.herokuapp.com/profile')
+      //axios.get("http://localhost:3000/profile")
+      .then(response => setProfiles(response.data),
+          err => console.log(err)
+      ).catch(error => console.error(error))
+}
+
+//route to get players
+const getAllPlayers = () => {
+    axios.get('https://www.balldontlie.io/api/v1/stats')
+        .then(response => setAllPlayers(response.data),
+            err => console.log(err)
+        ).catch(error => console.error(error))
+}
+
+//Read Route
+// const getBooks = () => {
+//   axios.get('https://ga-bookstore-backend.herokuapp.com/api/books')
+//  //  axios.get("http://localhost:8000/api/books")
+//   .then(response => setBooks(response.data),
+//   err=> console.log(err)
+// )
+// .catch(error=> console.error(error))
+// }
+
+//Create Route for books
+// const handleCreate = (addBook) => {
+//  axios.post('https://ga-bookstore-backend.herokuapp.com/api/books', addBook)
+//  // axios.post("http://localhost:8000/api/books", addBook)
+//  .then((response) => {
+//    setBooks([...books, response.data])
+//  })
+// }
+
+//handles profile log in request
+const handleSignIn = (profileObj) => {
+   axios.post(`https://twentyfiveftfromtherim.herokuapp.com/login`, profileObj)
+       .then((response) => {
+           console.log(response);
+           console.log(response.data);
+           setLoggedIn(response.data)
+       })
+}
+
+//handles profile log out request
+const handleSignOut = (profileObj) => {
+   axios.post(`https://twentyfiveftfromtherim.herokuapp.com/logout`, profileObj)
+       .then((response) => {
+           console.log(response);
+           console.log(response.data);
+           setLoggedIn(response.data)
+       })
+}
+
+//create new user registration
+const handleRegistration = (newProfile) => {
+   console.log(newProfile);
+   axios.post('https://twentyfiveftfromtherim.herokuapp.com/register', newProfile)
+   .then((response) => {
+       console.log(response);
+       setProfiles([...profiles, response.data])
+   })
+}
+
+//update profile
+const handleUpdate = (editProfile) => {
+ axios.put('https://twentyfiveftfromtherim.herokuapp.com/:id' + editProfile.id, editProfile)
+ .then((response) => {
+   setProfiles(profiles.map((profile) => {
+     return profile.id !== response.data.id ? profile : response.data
+   }))
+ })
+}
+
+//Delete Route
+const handleDelete = (deletedProfile) => {
+ axios.delete('https://twentyfiveftfromtherim.herokuapp.com/:id' + deletedProfile.id)
+ // axios.delete('http://localhost:3000/:id' + deletedProfile.id)
+ .then((response) => {
+   setProfiles(profiles.filter(profile => profile.id !== deletedProfile.id))
+ })
+}
+
+ // Logs the user out
+ const handleLogout = () => {
+  setLoggedIn({})
+}
+
+
+//Gets all data then loads page
   useEffect(() => {
     // getBooks()
-    // getBookreviews()
-    getUserAccounts()
+    getAllPlayers()
+    getProfiles()
   }, [])
   
 
@@ -40,6 +141,15 @@ function App() {
         <div>
         
     <ToggleDivImage/>
+    <NewProfile
+      handleRegistration={handleRegistration}
+      signInToggle={signInToggle}
+      showSignIn={showSignIn}
+      signedIn={signedIn}
+      handleSignIn={handleSignIn}
+      handleLogout={handleLogout}
+      loggedIn={loggedIn}
+      />
 
    
          
--- src/components/ToggleImg.js
+++ src/components/ToggleImg.js
@@ -2,6 +2,7 @@ import react from 'react';
 import {useState} from 'react';
 
 const ToggleDivImage = () => {
+    // let image = require("./GettyImages-1173556.jpeg")
 
     /* Setup component state that tracks visibility of the image. Initially, we'll set
     the image to visible (toggled true) */
@@ -16,7 +17,7 @@ const ToggleDivImage = () => {
         <button onClick={toggleImage}>Toggle court</button>
         
         {/* Render image if toggled is truthy */}
-        {toggled && <img src="https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2017%2F0217%2Fr181550_1023x576_16%2D9.jpg&w=1140&cquality=40&format=jpg" alt="Cat" />}
+        {toggled && <img src={image} />}
       </div>
     );
   };
